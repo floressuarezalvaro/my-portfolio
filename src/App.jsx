1c83dd237ceb3e101597ad60ab11d34336dd8f16
Alvy <floressuarezalvaro@gmail.com>
2025-01-07 19:56:09 -0500

    updated styling to take up full page:

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import './App.css'
-
-import ResponsiveAppBar from './components/Navbar'
+import './App.css';
+import ResponsiveAppBar from './components/Navbar';
 
 function App() {
-
   return (
-    <>
+    <div className="App">
       <ResponsiveAppBar />
-      <h1>My Portfolio</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
-  )
+      <main>
+        <h1>My Portfolio</h1>
+        <div className="card">
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
