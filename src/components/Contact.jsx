117cd4e1dc6d29bf6bf5eaf3b2f2abee27ed09fb
Alvy <floressuarezalvaro@gmail.com>
2025-02-02 11:14:10 -0500

    updated contact link to open new tabs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,8 +4,20 @@ function Contact() {
       <a href='mailto:floressuarezalvaro@gmail.com'>
         floressuarezalvaro@gmail.com
       </a>
-      <a href='https://www.linkedin.com/in/afloressuarez/'> LinkedIn</a>
-      <a href='https://www.github.com/afloressuarez/'> Github</a>
+      <a
+        href='https://www.linkedin.com/in/afloressuarez/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        LinkedIn
+      </a>
+      <a
+        href='https://www.github.com/floressuarezalvaro/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Github
+      </a>
     </div>
   );
 }
